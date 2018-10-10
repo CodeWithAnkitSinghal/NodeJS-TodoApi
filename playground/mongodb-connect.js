2b3bbfde45f05bb0a4c43a91fd5ab26e2eca4751
Ankit Singhal <singhalankit769@gmail.com>
2018-10-10 19:48:18 +0530

    fetching data and count from a collection

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,6 +1,6 @@
-const mongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
-mongoClient.connect('mongodb://localhost:27017/ToDoApp',(err, client) => {
+MongoClient.connect('mongodb://localhost:27017/ToDoApp',(err, client) => {
     if(err){
         console.log('Could not connect to mongoDB');
         return ;
@@ -9,28 +9,51 @@ mongoClient.connect('mongodb://localhost:27017/ToDoApp',(err, client) => {
     }
     const db = client.db('TodoApp');
 
-    db.collection('Todos').insertOne({
-        text: 'Something to do',
-        completed: false
-    }, (err, result) => {
-        if(err){
-            return console.log('Unable to insert todo', err);
-        } else {
-            console.log(JSON.stringify(result.ops, undefined, 2));
-        }
-    });
+//insert data
 
-    db.collection('Users').insertOne({
-        name: 'Ankit Singhal',
-        age: 26,
-        location: 'gurgaon'
-    }, (err, result) => {
-        if(err){
-            return console.log('Unable to insert todo', err);
-        } else {
-            console.log(JSON.stringify(result.ops, undefined, 2));
-        }
-    });
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log('Unable to insert todo', err);
+    //     } else {
+    //         console.log(JSON.stringify(result.ops, undefined, 2));
+    //     }
+    // });
 
-    client.close();
+    // db.collection('Users').insertOne({
+    //     name: 'Ankit Singhal',
+    //     age: 26,
+    //     location: 'gurgaon'
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log('Unable to insert todo', err);
+    //     } else {
+    //         console.log(JSON.stringify(result.ops, undefined, 2));
+    //     }
+    // });
+
+//fetch data
+
+    // db.collection('Todos').find({_id: new ObjectID('5bbdeb7acf612c2944fd4692')}).toArray()
+    // .then((docs) => {
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch data', err);
+    // })
+    
+    //fetch count of documents in a collection
+    db.collection('Todos')
+    .find()
+    .count()
+    .then((count) => {
+        console.log(`Todos Count: ${count}`);
+    }, (err) => {
+        console.log('Unable to fetch data', err);
+    })
+
+//closing the mongoClient connection
+
+    //client.close();
 });
