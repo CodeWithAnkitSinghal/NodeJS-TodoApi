4d3a836b6c942462267384f1fb5a443b50c30101
Ankit Singhal <singhalankit769@gmail.com>
2018-10-11 14:56:32 +0530

    Creatign a new model | implementing validations on model schema

--- server/server.js
+++ server/server.js
@@ -5,18 +5,23 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        require: true,
+        minlenght: 5,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
 var newTodo = new Todo({
-    text: 'Cook dinner',
+    text: 'Cook dinner at 2',
     completed: 'false'
 });
 
@@ -38,3 +43,24 @@ newTodo.save()
 // }, (e) => {
 //     console.log('Unable to save todo');
 // });
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        require: true,
+        minlenght: 5,
+        trim: true
+    }
+});
+
+var newUser = new User({
+    email: 'aaa@aaa.com'
+});
+
+newUser.save()
+.then((docs) => {
+    console.log(`User saved`);
+    console.log(JSON.stringify(docs, undefined, 2));
+}, (err) => {
+    console.log(`unable to save`, err);
+})
